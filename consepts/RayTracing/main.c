d6b51c60661ec5ccd77d7015d4bd5130a7c79cd4
Alberto Hamuyela <ahamuyel@c1r4s1.42luanda.com>
2025-04-14 12:33:57 +0100

    addin' map

--- consepts/RayTracing/main.c
+++ consepts/RayTracing/main.c
@@ -17,7 +17,7 @@ void	clear_screan(t_data *data)
 	{
 		x = 0;
 		while (x < WIDTH)
-			my_put_mlx(x++, y, 0x0ffff0, data);
+			my_put_mlx(x++, y, 0x0fffff, data);
 		y++;
 	}
 }
@@ -32,7 +32,24 @@ void	draw_player(t_data *data)
 	{
 		i = 0;
 		while (i < SIZE)
-			my_put_mlx(data->x + i++, data->y + j, 0xff00f0, data);
+			my_put_mlx(data->x + i++, data->y + j, 0xf00000, data);
+		j++;
+	}
+}
+
+
+
+void	draw_block(t_data *data, int x, int y, int color)
+{
+	int	j;
+	int	i;
+
+	j = 0;
+	while (j < BLOCK)
+	{
+		i = 0;
+		while (i < BLOCK)
+			my_put_mlx(x * BLOCK + i++, y * BLOCK + j, color, data);
 		j++;
 	}
 }
@@ -51,9 +68,31 @@ int	handle_key(int keycode, t_data *data)
 	return (0);
 }
 
+void	draw_map(t_data *data)
+{
+	int	color;
+
+	int x, y = 0;
+	while (y < MAP_H)
+	{
+		x = 0;
+		while (x < MAP_W)
+		{
+			if (data->map[y][x] == '1')
+				color = 0xffffff;
+			else 
+				color = 0x000000;
+			draw_block(data, x, y, color);
+			x++;
+		}
+		y++;
+	}
+}
+
 int	render_frame(t_data *data)
 {
 	clear_screan(data);
+	draw_map(data);
 	draw_player(data);
 	mlx_put_image_to_window(data->mlx, data->win, data->img, 0, 0);
 	return (0);
@@ -61,6 +100,10 @@ int	render_frame(t_data *data)
 
 void	init_game(t_data *data)
 {
+	char	tmp_map[MAP_H][MAP_W] = {"1111111111", "1000000001", "1011001101",
+			"1010000101", "1000110001", "1000110001", "1010000101",
+			"1011001101", "1000000001", "1111111111"};
+
 	data->mlx = mlx_init();
 	data->win = mlx_new_window(data->mlx, WIDTH, HEIGHT, "RayTracing");
 	data->img = mlx_new_image(data->mlx, WIDTH, HEIGHT);
@@ -68,6 +111,18 @@ void	init_game(t_data *data)
 			&data->endian);
 	data->x = WIDTH / 2 - SIZE / 2;
 	data->y = HEIGHT / 2 - SIZE / 2;
+	int x, y = 0;
+	while (y < MAP_H)
+	{
+		x = 0;
+		while (x < MAP_W)
+		{
+			data->map[y][x] = tmp_map[y][x];
+			x++;
+		}
+		data->map[y][x] = '\0';
+		y++;
+	}
 }
 
 int	main(void)
--- consepts/RayTracing/rayTracing.h
+++ consepts/RayTracing/rayTracing.h
@@ -8,16 +8,22 @@
 # include <stdio.h>
 # include <stdlib.h>
 
-# define WIDTH 1200
-# define HEIGHT 800
+# define WIDTH 640
+# define HEIGHT 640
 # define BLOCK 64
-# define SIZE 64
+# define SIZE 20
 
 # define KEY_W 119
 # define KEY_S 115
 # define KEY_A 97
 # define KEY_D 100
-# define SPEED 60
+# define SPEED 20
+
+# define KEY_Q 118
+# define KEY_E 120
+
+# define MAP_W 10
+# define MAP_H 10
 
 typedef struct s_data
 {
@@ -28,8 +34,13 @@ typedef struct s_data
 	int		line_length;
 	int		bbp;
 	int		endian;
+
 	int		x;
 	int		y;
+	float	angle;
+	float	dir_x;
+	float	dir_y;
+	char	map[MAP_H][MAP_W + 1];
 }			t_data;
 
 // Player
